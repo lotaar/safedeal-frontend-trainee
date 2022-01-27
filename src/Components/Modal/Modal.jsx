c441564397f2ef791d35e90c70816ab6be8cbe24
lotaar <remnantnogosu@gmail.com>
2022-01-27 05:31:16 +0300

    wip 3

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,60 +1,111 @@
 import React, { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
-import style from '../Modal/Modal.module.css'
+import style from "../Modal/Modal.module.css";
 import Preloader from "../Preloader/Preloader";
-let Modal = ({active, setActive, id}) => {
 
-    const [modalContentData, setModalContentData] = useState('')
-    const [isLoading, setIsLoading] =useState(true)
-     
-    useEffect(() => {
-        fetch(`https://boiling-refuge-66454.herokuapp.com/images/${id}`)
-          .then((response) => response.json())
-          .then((data) => setModalContentData(data))
-          .then(()=>setIsLoading(false))
-      }, [id]);
-      console.log(modalContentData);
-    return (
-        <div className={active? style.modalActive : style.modal}>
-            <div className={style.modalContent}>
-                <div className={style.imageWithForm}>
-                    <img src={isLoading? "Loading": modalContentData.url} alt="Сиськи"/>
-                    <form>
-                        <input placeholder="Ваше имя"></input>
-                        <br/>
-                        <input placeholder="Ваш комментарий"></input>
-                        <br/>
-                        <button>Оценить Сиськи</button>
-                    </form>
-                </div>
-                {isLoading? "Loading" : modalContentData.comments.map((comment) => {
-                   return(
-                       <div>
-                           <p>{comment.date}</p>
-                           <p>{comment.text}</p>
-                       </div>
-                   )
-                })}
-                <button className={style.closeButton} onClick={()=> setActive(false)}>×</button>
-            </div>
+const initialForm = {
+  name: "",
+  comment: "",
+};
+
+let Modal = ({ active, setActive, id }) => {
+  const [modalContentData, setModalContentData] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [formData, setFormData] = useState(initialForm);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const apiFormData = new FormData()
+    apiFormData.append('name', formData.name)
+    apiFormData.append('comment', formData.comment)
+
+    fetch(
+      `https://boiling-refuge-66454.herokuapp.com/images/${id}/comments`,
+      {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: apiFormData,
+      }
+    ).then((res) => res.json()).then((data) => console.log(data))
+  }
+
+  useEffect(() => {
+    fetch(`https://boiling-refuge-66454.herokuapp.com/images/${id}`)
+      .then((response) => response.json())
+      .then((data) => setModalContentData(data))
+      .then(() => setIsLoading(false));
+  }, [id]);
+  console.log(modalContentData);
+
+  const handleChangeField = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className={active ? style.modalActive : style.modal}>
+      <div className={style.modalContent}>
+        <div className={style.imageWithForm}>
+          <img
+            src={isLoading ? "Loading" : modalContentData.url}
+            alt="Сиськи"
+          />
+          <form>
+            <input
+              placeholder="Ваше имя"
+              name="name"
+              value={formData.name}
+              onChange={handleChangeField}
+            />
+            <input
+              placeholder="Ваше комментарий"
+              name="comment"
+              value={formData.comment}
+              onChange={handleChangeField}
+            />
+
+            <button onClick={handleSubmit}>Оценить сиськи</button>
+          </form>
         </div>
-    )
-}
+        {isLoading
+          ? "Loading"
+          : modalContentData.comments.map((comment) => {
+              return (
+                <div>
+                  <p>{comment.date}</p>
+                  <p>{comment.text}</p>
+                </div>
+              );
+            })}
+        <button className={style.closeButton} onClick={() => setActive(false)}>
+          ×
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Modal
+export default Modal;
 
-{/* <div className={style.comments}>
+{
+  /* <div className={style.comments}>
 
 
 <p className={style.commentData}>02.02.03</p>
 <p className={style.commentText}>Сиськи хорошие</p> 
                    
-               </div> */}
+               </div> */
+}
 
-{/* <p className={style.commentData}>02.02.03</p>
-<p className={style.commentText}>Сиськи хорошие</p> */}
+{
+  /* <p className={style.commentData}>02.02.03</p>
+<p className={style.commentText}>Сиськи хорошие</p> */
+}
 
-{/* <div className={active? style.modalActive : style.modal}>
+{
+  /* <div className={active? style.modalActive : style.modal}>
 <div className={style.modalContent}>
     <div className={style.modalImage}>
 
@@ -68,4 +119,5 @@ export default Modal
     </form>
     <button onClick={()=> setActive(false)}>×</button>
 </div>
-</div> */}
+</div> */
+}
